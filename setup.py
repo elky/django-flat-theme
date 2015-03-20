c104f5e449341f4ab01d8c1ff13f175f28a92065
elky <mansellfan@gmail.com>
2015-03-20 21:30:52 +0500

    Version update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description = ('A flat theme for Django admin interface. Modern, fresh, simple.'),
     license = 'BSD',
     url = 'https://github.com/elky/django-flat-theme',
-    download_url = 'https://github.com/elky/django-flat-theme/tarball/0.9.1',
+    download_url = 'https://github.com/elky/django-flat-theme/tarball/0.9.2',
     keywords = ['django', 'admin', 'theme', 'interface'],
     include_package_data = True,
 )
